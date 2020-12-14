e7dd7eb415b91c954bd8ff1af1bf32160ef7b1d9
黄圣彪 <huangshengbiao@bilibili.com>
2020-12-14 18:24:48 +0800

    refactor org export pdf config

--- lisp/org/init-org-to-pdf.el
+++ lisp/org/init-org-to-pdf.el
@@ -42,39 +42,50 @@
 (unless (boundp 'org-latex-classes)
   (setq org-latex-classes nil))
 
-(add-to-list 'org-latex-classes '("cn-article"
-                                  "{{{cn-article-class}}}
+(bh/add-variables-to-list 'org-latex-classes
+                          '("cn-article"
+                            "{{{cn-article-class}}}
                                    [NO-DEFAULT-PACKAGES]
                                    [NO-PACKAGES]"
-                                  ("\\section{%s}" . "\\section*{%s}")
-                                  ("\\subsection{%s}" . "\\subsection*{%s}")
-                                  ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-                                  ("\\paragraph{%s}" . "\\paragraph*{%s}")
-                                  ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+                            ("\\section{%s}" . "\\section*{%s}")
+                            ("\\subsection{%s}" . "\\subsection*{%s}")
+                            ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                            ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                            ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
+                          
+                          '("cn-report"
+                            "{{{cn-report-class}}}
+                                   [NO-DEFAULT-PACKAGES]
+                                   [NO-PACKAGES]"
+                            ("\\part{%s}" . "\\part*{%s}")
+                            ("\\chapter{%s}" . "\\chapter*{%s}")
+                            ("\\section{%s}" . "\\section*{%s}")
+                            ("\\subsection{%s}" . "\\subsection*{%s}")
+                            ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                            ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                            ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
 
-(add-to-list 'org-latex-classes '("cn-report"
-                                  "{{{cn-report-class}}}
+                          '("cn-book"
+                            "{{{cn-book-class}}}
                                    [NO-DEFAULT-PACKAGES]
                                    [NO-PACKAGES]"
-                                  ("\\part{%s}" . "\\part*{%s}")
-                                  ("\\chapter{%s}" . "\\chapter*{%s}")
-                                  ("\\section{%s}" . "\\section*{%s}")
-                                  ("\\subsection{%s}" . "\\subsection*{%s}")
-                                  ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-                                  ("\\paragraph{%s}" . "\\paragraph*{%s}")
-                                  ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+                            ("\\part{%s}" . "\\part*{%s}")
+                            ("\\chapter{%s}" . "\\chapter*{%s}")
+                            ("\\section{%s}" . "\\section*{%s}")
+                            ("\\subsection{%s}" . "\\subsection*{%s}")
+                            ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                            ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                            ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
 
-(add-to-list 'org-latex-classes '("cn-book"
-                                  "{{{cn-book-class}}}
+                          '("cn-beamer"
+                            "{{{cn-beamer-class}}}
                                    [NO-DEFAULT-PACKAGES]
                                    [NO-PACKAGES]"
-                                  ("\\part{%s}" . "\\part*{%s}")
-                                  ("\\chapter{%s}" . "\\chapter*{%s}")
-                                  ("\\section{%s}" . "\\section*{%s}")
-                                  ("\\subsection{%s}" . "\\subsection*{%s}")
-                                  ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-                                  ("\\paragraph{%s}" . "\\paragraph*{%s}")
-                                  ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+                            ("\\section{%s}" . "\\section*{%s}")
+                            ("\\subsection{%s}" . "\\subsection*{%s}")
+                            ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                            )
+                          )
 
 ;; 使用Listings宏包格式化源代码(只是把代码框用listing环境框起来，还需要额外的设置)
 (setq org-latex-listings t)
@@ -84,63 +95,24 @@
 ;; Make Org use ido-completing-read for most of its completing prompts.
 (setq org-completion-use-ido t)
 
-
-;; 导出Beamer的设置
-;; allow for export=>beamer by placing #+LaTeX_CLASS: beamer in org files
-;;-----------------------------------------------------------------------------
-(add-to-list 'org-latex-classes '("cn-beamer"
-                                  "{{{cn-beamer-class}}}
-                                   [NO-DEFAULT-PACKAGES]
-                                   [NO-PACKAGES]"
-                                  ("\\section{%s}" . "\\section*{%s}")
-                                  ("\\subsection{%s}" . "\\subsection*{%s}")
-                                  ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-                                  ))
-
-(defun replace-cn-article-class (contents backend info)
+(defun replace-cn-class (contents backend info)
   "Replace cn article marco.
 CONTENTS source string.
 BACKEND backend.
 INFO info."
   (interactive)
-  (when  (org-export-derived-backend-p backend 'latex)
-    (when (string-match "{{{cn-article-class}}}" contents)
-      (replace-regexp-in-string "{{{cn-article-class}}}" (f-read-text cn-article-class-file) contents))))
+  (when (org-export-derived-backend-p backend 'latex)
+    (cond ((string-match "{{{cn-article-class}}}" contents)
+           (replace-regexp-in-string "{{{cn-article-class}}}" (f-read-text cn-article-class-file) contents))
+          ((string-match "{{{cn-report-class}}}" contents)
+           (replace-regexp-in-string "{{{cn-report-class}}}" (f-read-text cn-report-class-file) contents))
+          ((string-match "{{{cn-book-class}}}" contents)
+           (replace-regexp-in-string "{{{cn-book-class}}}" (f-read-text cn-book-class-file) contents))
+          ((string-match "{{{cn-beamer-class}}}" contents)
+           (replace-regexp-in-string "{{{cn-beamer-class}}}" (f-read-text cn-beamer-class-file) contents))
+          )))
 
-(defun replace-cn-report-class (contents backend info)
-  "Replace cn article marco.
-CONTENTS source string.
-BACKEND backend.
-INFO info."
-  (interactive)
-  (when  (org-export-derived-backend-p backend 'latex)
-    (when (string-match "{{{cn-report-class}}}" contents)
-      (replace-regexp-in-string "{{{cn-report-class}}}" (f-read-text cn-report-class-file) contents))))
-
-(defun replace-cn-book-class (contents backend info)
-  "Replace cn article marco.
-CONTENTS source string.
-BACKEND backend.
-INFO info."
-  (interactive)
-  (when  (org-export-derived-backend-p backend 'latex)
-    (when (string-match "{{{cn-book-class}}}" contents)
-      (replace-regexp-in-string "{{{cn-book-class}}}" (f-read-text cn-book-class-file) contents))))
-
-(defun replace-cn-beamer-class (contents backend info)
-  "Replace cn article marco.
-CONTENTS source string.
-BACKEND backend.
-INFO info."
-  (interactive)
-  (when  (org-export-derived-backend-p backend 'latex)
-    (when (string-match "{{{cn-beamer-class}}}" contents)
-      (replace-regexp-in-string "{{{cn-beamer-class}}}" (f-read-text cn-beamer-class-file) contents))))
-
-(add-to-list 'org-export-filter-final-output-functions 'replace-cn-article-class)
-(add-to-list 'org-export-filter-final-output-functions 'replace-cn-report-class)
-(add-to-list 'org-export-filter-final-output-functions 'replace-cn-book-class)
-(add-to-list 'org-export-filter-final-output-functions 'replace-cn-beamer-class)
+(add-to-list 'org-export-filter-final-output-functions 'replace-cn-class)
 
 (setq ps-paper-type 'a4
       ps-font-size 16.0
